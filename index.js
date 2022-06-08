d7bc5771901e9a37019e12817c0f88719c5df7df
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-06-08 18:38:08 +0530

    updated with porxy sever and solved many bug

--- index.js
+++ index.js
@@ -1,62 +1,69 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs/promises");
-const express = require("express");
-const app = express();
 
-const port = 5000;
-app.listen(port, () => {
-  console.log("listening on Port " + port);
-});
-
-//server is ready so when we make req with below api
-
-app.get("/", async (req, res) => {
-  try {
-    //localhost:5000?location=Chinatown Complex - 335 Smith St, Singapore, 050335
-    let location = req.query.location;
-    if (location) {
-      start(location);
+//I am able to catch network req wich contain
+/*
+searchResult:{
+  searchMerchants:[
+    {
+      latlng: {latitude: 1.2857211372800634, longitude: 103.84982956837393}
     }
-  } catch (err) {
-    return res.status(400).send({ message: err.message });
-  }
-});
-
-async function start(location) {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--proxy-server=83.77.118.53:17171"],
-  });
-  /*
-now server is completed but its
-creating an error
-*/
-  const page = await browser.newPage();
-
-  const webUrl = "https://food.grab.com";
-
-  page.on("response", async (res) => {
-    //the data we can recieve here if browser make any requests
-    //we need to apply some logic to get longitude and latitue
-    console.log(await res._request._resourceType);
-
-    // we need to send the res that key word is search contain this type of data
-
-    // now we need to make some simple logic to collect latitude and longitude
-    return res.status(200).send({ data: res._request._resourceType });
-  });
-  await page.goto(webUrl);
-  await page.screenshot({ path: "amazing.png", fullPage: true });
-
-  //here page.type set the location with api location
-  await page.type("#location-input", location);
-
-  //here search button is fired
-  await page.click(".ant-btn");
-
-  //here we are wating to full fill
-  await Promise.all([page.click(".ant-btn button"), page.waitForNavigation()]);
-  //when promis got full fill it return the data from line event emiiter that is set above page.on res
-  //here we close the data
-  await browser.close();
+  ]
 }
+
+Like this i found this in developer tool network tab
+
+*/
+
+//this is a selp calling funtion
+// run -- node index.js to test the code
+
+(async () => {
+  const browser = await puppeteer.launch({
+    //below is the proxy server so that we can scrap it from anywhere
+    args: ["--no-sandbox", "--proxy-server=110.170.126.13:3128"],
+    headless: false,
+    ignoreHTTPSErrors: true,
+  });
+  const page = await browser.newPage();
+  const webUrl = "https://food.grab.com/sg/en/";
+
+  try {
+    //here we got that url that is assign on webUrl line 30
+    await page.goto(webUrl);
+    //here we can take screenshot also
+    await page.screenshot({ path: "screen.png", fullPage: true });
+
+    let location = "Chinatown Complex - 335 Smith St, Singapore, 050335";
+    await page.type("#location-input", location);
+    //here page.type type location in input tab on frontend
+
+    await Promise.all([
+      page.click(
+        "#page-content > div.sectionContainer___3GDBD.searchSectionContainer___3Lhkk.ant-layout > div > button"
+      ),
+      //here search button is fired
+
+      page.waitForNavigation(),
+    ]);
+    //here we are wating to full fill
+
+    await page.screenshot({ path: "restoraunt.png", fullPage: true });
+
+    //this is the event which is listening for every req that frontend make
+    page.on("response", async (res) => {
+      //the data we can recieve here if browser make any requests
+      //we need to apply some logic to get longitude and latitue
+      if (res.url() == "https://portal.grab.com/foodweb/v2/search") {
+        let respond = await res._request;
+        console.log(respond);
+        //here I  got the request but i am not able to extract it
+      }
+      // now we need to make some logic to collect data and then latitude and longitude
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+  //here we close the browser
+  // await page.close();
+  // await browser.close();
+})();
